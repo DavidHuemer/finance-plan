92ee86cf3d680fa1f71991d67d396695775dcd81
DavidHuemer <92david.huemer@gmail.com>
2024-03-16 20:25:28 +0100

    Added basic auth setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from "@angular/fire/auth-guard";
 
-export const routes: Routes = [];
+const redirectUnauthorizedToAuth = () => redirectUnauthorizedTo(['auth/sign-in']);
+const redirectLoggedInToMain = () => redirectLoggedInTo(['']);
+
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    ...canActivate(redirectLoggedInToMain)
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule),
+    ...canActivate(redirectUnauthorizedToAuth)
+  },
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+];
